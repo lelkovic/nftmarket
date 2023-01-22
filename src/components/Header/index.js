47b24c134c538eef65f7b7b5d0b4e8745335ce0d
lelkovic <gladv1762@gmail.com>
2023-01-22 14:02:36 +0200

    upd header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,28 @@
 import React from "react";
+import Logo from "../../assets/images/logo.svg";
 import styles from "./styles.module.scss";
 
 export const Header = () => { 
     return (
-        <div className={styles.header}>
-            <p>
-                Header
-            </p>
-            header
-        </div>
+        <header className={styles.header}>
+            <img src={Logo} alt="logo" />
+            <div className={styles.header__sep}></div>
+            <nav>
+                <a href="#Discover">
+                    Discover
+                </a>
+                <a href="#How-it-work">
+                    How it work
+                </a>
+                <input type="text" placeholder="Search" />
+                <button className={styles.header__bbtn}>
+                    Upload
+                </button>
+                <button className={styles.header__wbtn}>
+                    Connect Wallet
+                </button>
+            </nav>
+        </header>
     );
 };
 
