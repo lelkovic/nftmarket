7649b283cf6512d61df27cd964616770a1ec4d99
lelkovic <gladv1762@gmail.com>
2023-02-11 18:06:11 +0200

    Update Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,56 @@
 import React from "react";
+import Container from "../Container";
+import Logo from "../../assets/images/logo.png";
+import styles from "./styles.module.scss";
 
 export const Footer = () => {
     return (
-        <div 
-        // className={styles.footer}
-        >
-            footer
-        </div>
+        <Container className={styles.footer}>
+            <div className={styles.footer__top}>
+                <div>
+                    <img src={Logo} alt="logo" />
+                    <h3>The New Creative Economy.</h3>
+                </div>
+                <nav>
+                    <ul>
+                        <li>Crypter.</li>
+                        <li>
+                            <a href="#">Discover</a>
+                        </li>
+                        <li>
+                            <a href="#">Connect wallet</a>
+                        </li>
+                        <li>
+                            <a href="#">Create item</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>Info</li>
+                        <li>
+                            <a href="#">Download</a>
+                        </li>
+                        <li>
+                            <a href="#">Demos</a>
+                        </li>
+                        <li>
+                            <a href="#">Support</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div>
+                    <p>Join Newsletter</p>
+                    <p>Subscribe our newsletter to get more free design course and resource</p>
+                    <input type="text" placeholder="Enter your email" />
+                    <button>{"->"}</button>
+                </div>
+            </div>
+            <div className={styles.footer__bottom}>
+                <p className={styles.footer__copy}>Copyright © 2021 UI8 LLC. All rights reserved</p>
+                <p className={styles.footer__cookies}>
+                    We use cookies for better service. <span>Accept</span>
+                </p>
+            </div>
+        </Container>
     );
 };
 
