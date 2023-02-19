61598a1af3c1002c0a9b1604d30f326fab4b4860
lelkovic <gladv1762@gmail.com>
2023-02-19 17:18:30 +0200

    upd titleBlock

--- src/components/TitleBlock/index.js
+++ src/components/TitleBlock/index.js
@@ -7,9 +7,9 @@ export const TitleBlock = () => {
     return (
         <Container>
             <div className={styles.titleBlock}>
-                <h3>Create, explore, & collect digital art NFTs.</h3>
-                <h2>The new creative economy.</h2>
-                <button>Start your search</button>
+                <h3 className={styles.titleBlock__subtitle}>Create, explore, & collect digital art NFTs.</h3>
+                <h2 className={styles.titleBlock__title}>The new creative economy.</h2>
+                <button className={styles.titleBlock__button}>Start your search</button>
             </div>
         </Container>
     );
