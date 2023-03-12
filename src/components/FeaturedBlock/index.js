1e3d55713453f0245c5c8aa9fe376bd0e4ad768d
lelkovic <gladv1762@gmail.com>
2023-03-12 12:05:21 +0200

    Featured Block upd no styles

--- src/components/FeaturedBlock/index.js
+++ src/components/FeaturedBlock/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import Container from "../Container";
 import featuredBlock from "../../assets/images/featuredBlock.png";
 import styles from "./styles.module.scss";
+import avatar from "../../assets/images/avatar.png"
+import user from "../../assets/images/user.png"
 
 export const FeaturedBlock = () => {
     return (
@@ -12,7 +14,26 @@ export const FeaturedBlock = () => {
             <div>
                 <img src={featuredBlock} alt="featuredblock" />
             </div>
+
+            <div className={styles.featuredBlock__creatortitle}>
+                Creator
+
+            </div>
+
+            <div className={styles.featuredBlock__creatorsubtitle}>Enrico Cole</div>
+            <img src={avatar} alt="Avatar" />
+
+            <div className={styles.featuredBlock__usertitle}>Instant price</div>
+
+            <div className={styles.featuredBlock__usersubtitle}>3.5 ETH</div>
+            <img src={user} alt="userloading"></img>
+
+            <a className={styles.featuredBlock__buttontop}>Place a bid</a>
+                
+
+            <a className={styles.featuredBlock__buttonbot}>View item</a>
         </Container>
+        
     );
 };
 
