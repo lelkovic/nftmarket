bea79ad9edcbaefa8c2bb226e3846ad79455e603
lelkovic <gladv1762@gmail.com>
2023-03-12 13:03:26 +0200

    Featured Block styles upd

--- src/components/FeaturedBlock/index.js
+++ src/components/FeaturedBlock/index.js
@@ -2,38 +2,42 @@ import React from "react";
 import Container from "../Container";
 import featuredBlock from "../../assets/images/featuredBlock.png";
 import styles from "./styles.module.scss";
-import avatar from "../../assets/images/avatar.png"
-import user from "../../assets/images/user.png"
+import avatar from "../../assets/images/avatar.png";
+import user from "../../assets/images/user.png";
 
 export const FeaturedBlock = () => {
     return (
         <Container>
-            <div className={styles.featuredBlock}> 
-                <h2 className={styles.featuredBlock__title}>Marco carrillo®</h2>
+            <div className={styles.featuredBlock}>
+                <div className={styles.featuredBlock__left}>
+                    <img src={featuredBlock} alt="featuredblock" />
+                </div>
+                <div className={styles.featuredBlock__right}>
+                    <h2 className={styles.featuredBlock__title}>Marco carrillo®</h2>
+
+                    <div classname={styles.featuredBlock__subInfo}>
+                        <div className={styles.featuredBlock__creator}>
+                            <img src={avatar} alt="Avatar" />
+                            <div>
+                                <p className={styles.featuredBlock__subInfoTitle}>Creator</p>
+                                <p className={styles.featuredBlock__subInfoText}>Enrico Cole</p>
+                            </div>
+                        </div>
+
+                        <div className={styles.featuredBlock__instantPrice}>
+                            <img src={user} alt="userloading"></img>
+                            <div>
+                                <p className={styles.featuredBlock__subInfoTitle}>Instant Price</p>
+                                <p className={styles.featuredBlock__subInfoText}>3.5 ETH</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a className={styles.featuredBlock__buttontop}>Place a bid</a>
+                    <a className={styles.featuredBlock__buttonbot}>View item</a>
+                </div>
             </div>
-            <div>
-                <img src={featuredBlock} alt="featuredblock" />
-            </div>
-
-            <div className={styles.featuredBlock__creatortitle}>
-                Creator
-
-            </div>
-
-            <div className={styles.featuredBlock__creatorsubtitle}>Enrico Cole</div>
-            <img src={avatar} alt="Avatar" />
-
-            <div className={styles.featuredBlock__usertitle}>Instant price</div>
-
-            <div className={styles.featuredBlock__usersubtitle}>3.5 ETH</div>
-            <img src={user} alt="userloading"></img>
-
-            <a className={styles.featuredBlock__buttontop}>Place a bid</a>
-                
-
-            <a className={styles.featuredBlock__buttonbot}>View item</a>
         </Container>
-        
     );
 };
 
