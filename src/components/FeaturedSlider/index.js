9fafe2520744cec2c7e65a823fb21b93da36b417
lelkovic <gladv1762@gmail.com>
2023-06-18 14:32:26 +0300

    slider upd

--- src/components/FeaturedSlider/index.js
+++ src/components/FeaturedSlider/index.js
@@ -38,6 +38,56 @@ export const FeaturedSlider = () => {
         slidesToShow: 1,
         slidesToScroll: 1,
     };
+
+    const renderSliderItems = () => {
+        return sliderData.map((item, key) => (
+            <div key={key}>
+                <div className={styles.featuredSlider}>
+                    <div className={styles.featuredSlider__left}>
+                        <img src={item.image} alt={item.title} />
+                    </div>
+                    <div className={styles.featuredSlider__right}>
+                        <h2 className={styles.featuredSlider__title}>{item.title}</h2>
+
+                        <div className={styles.featuredSlider__subInfo}>
+                            <div>
+                                <img className={styles.featuredSlider__avatar} src={avatar} alt="Avatar" />
+                                <div>
+                                    <p className={styles.featuredSlider__subInfoTitle}>Creator</p>
+                                    <p className={styles.featuredSlider__subInfoText}>{item.creator}</p>
+                                </div>
+                            </div>
+                            <div>
+                                <img className={styles.featuredSlider__avataruser} src={user} alt="userloading"></img>
+                                <div>
+                                    <p className={styles.featuredSlider__subInfoTitle}>Instant Price</p>
+                                    <p className={styles.featuredSlider__subInfoText}>{item.instantPrice} ETH</p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className={styles.featuredSlider__info}>
+                            <p className={styles.featuredSlider__bidTitle}>Current Bid</p>
+                            <p className={styles.featuredSlider__bidPrice}>{item.currentBid} ETH</p>
+                            <p className={styles.featuredSlider__usdPrice}>
+                                {data ? "$" + data.market_data.current_price.usd : "Loading.."}
+                            </p>
+                            <p className={styles.featuredSlider__countdown}>Auction ending in</p>
+                            <MyTimer />
+                        </div>
+
+                        <a href="#" className={styles.featuredSlider__buttontop}>
+                            Place a bid
+                        </a>
+                        <a href="#" className={styles.featuredSlider__buttonbot}>
+                            View item
+                        </a>
+                    </div>
+                </div>
+            </div>
+        ));
+    };
+
     useEffect(() => {
         const getData = async () => {
             const url = "https://api.coingecko.com/api/v3/coins/ethereum";
@@ -55,104 +105,7 @@ export const FeaturedSlider = () => {
 
     return (
         <Container>
-            <Slider {...settings}>
-                <div>
-                    <div className={styles.featuredSlider}>
-                        <div className={styles.featuredSlider__left}>
-                            <img src={featuredBlock} alt="featuredblock" />
-                        </div>
-                        <div className={styles.featuredSlider__right}>
-                            <h2 className={styles.featuredSlider__title}>Marco carrillo®</h2>
-
-                            <div className={styles.featuredSlider__subInfo}>
-                                <div>
-                                    <img className={styles.featuredSlider__avatar} src={avatar} alt="Avatar" />
-                                    <div>
-                                        <p className={styles.featuredSlider__subInfoTitle}>Creator</p>
-                                        <p className={styles.featuredSlider__subInfoText}>Enrico Cole</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <img
-                                        className={styles.featuredSlider__avataruser}
-                                        src={user}
-                                        alt="userloading"
-                                    ></img>
-                                    <div>
-                                        <p className={styles.featuredSlider__subInfoTitle}>Instant Price</p>
-                                        <p className={styles.featuredSlider__subInfoText}>3.5 ETH</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className={styles.featuredSlider__info}>
-                                <p className={styles.featuredSlider__bidTitle}>Current Bid</p>
-                                <p className={styles.featuredSlider__bidPrice}>1.00 ETH</p>
-                                <p className={styles.featuredSlider__usdPrice}>
-                                    {data ? "$" + data.market_data.current_price.usd : "Loading.."}
-                                </p>
-                                <p className={styles.featuredSlider__countdown}>Auction ending in</p>
-                                <MyTimer />
-                            </div>
-
-                            <a href="#" className={styles.featuredSlider__buttontop}>
-                                Place a bid
-                            </a>
-                            <a href="#" className={styles.featuredSlider__buttonbot}>
-                                View item
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div className={styles.featuredSlider}>
-                        <div className={styles.featuredSlider__left}>
-                            <img src={featuredBlock} alt="featuredblock" />
-                        </div>
-                        <div className={styles.featuredSlider__right}>
-                            <h2 className={styles.featuredSlider__title}>Slider 2</h2>
-
-                            <div className={styles.featuredSlider__subInfo}>
-                                <div>
-                                    <img className={styles.featuredSlider__avatar} src={avatar} alt="Avatar" />
-                                    <div>
-                                        <p className={styles.featuredSlider__subInfoTitle}>Creator</p>
-                                        <p className={styles.featuredSlider__subInfoText}>Enrico Cole</p>
-                                    </div>
-                                </div>
-                                <div>
-                                    <img
-                                        className={styles.featuredSlider__avataruser}
-                                        src={user}
-                                        alt="userloading"
-                                    ></img>
-                                    <div>
-                                        <p className={styles.featuredSlider__subInfoTitle}>Instant Price</p>
-                                        <p className={styles.featuredSlider__subInfoText}>3.5 ETH</p>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className={styles.featuredSlider__info}>
-                                <p className={styles.featuredSlider__bidTitle}>Current Bid</p>
-                                <p className={styles.featuredSlider__bidPrice}>1.00 ETH</p>
-                                <p className={styles.featuredSlider__usdPrice}>
-                                    {data ? "$" + data.market_data.current_price.usd : "Loading.."}
-                                </p>
-                                <p className={styles.featuredSlider__countdown}>Auction ending in</p>
-                                <MyTimer />
-                            </div>
-
-                            <a href="#" className={styles.featuredSlider__buttontop}>
-                                Place a bid
-                            </a>
-                            <a href="#" className={styles.featuredSlider__buttonbot}>
-                                View item
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </Slider>
+            <Slider {...settings}>{sliderData && renderSliderItems()}</Slider>
         </Container>
     );
 };
