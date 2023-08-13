a49b4064145d6a3f3e0d9616690f7c92cca8996b
lelkovic <gladv1762@gmail.com>
2023-08-13 15:39:17 +0300

    Disconnect wallet upd

--- src/components/Connect/index.js
+++ src/components/Connect/index.js
@@ -1,26 +1,31 @@
 import React from "react";
 import Container from "../Container";
 import styles from "./styles.module.scss";
+import { hooksMetaMask } from "../../web3-lib/connectors";
+import { handleDisconnect } from "../../helpers/web3";
 import connectPreview from "../../assets/images/connectPreview.png";
 
-import { metaMask } from '../../web3-lib/connectors'
+import { metaMask } from "../../web3-lib/connectors";
 // import { walletConnectV2 } from '../../web3-lib/connectors'
-import { getAddChainParameters } from '../../web3-lib/chains'
+import { getAddChainParameters } from "../../web3-lib/chains";
 
 export const Connect = () => {
+    const { useAccounts } = hooksMetaMask;
+    const accounts = useAccounts();
+
     const goBack = () => {
         window.history.back();
     };
 
     const desiredChainId = 1;
-  
+
     const switchChain = async (connector) => {
-      try {
-        await connector.activate(getAddChainParameters(desiredChainId))
-      } catch (error) {
-        console.log(error)
-      }
-    }
+        try {
+            await connector.activate(getAddChainParameters(desiredChainId));
+        } catch (error) {
+            console.log(error);
+        }
+    };
     return (
         <Container className={styles.connect}>
             <div className={styles.connect__header}>
@@ -31,9 +36,13 @@ export const Connect = () => {
             </div>
             <div className={styles.connect__main}>
                 <div className={styles.connect__left}>
-                    <button onClick={() => switchChain(metaMask)}>
-                        MetaMask
-                    </button>
+                    {accounts ? (
+                        <button className={styles.connect__disconnect} onClick={handleDisconnect}>
+                            Disconnect
+                        </button>
+                    ) : (
+                        <button onClick={() => switchChain(metaMask)}>MetaMask</button>
+                    )}
                     {/* <button onClick={() => switchChain(walletConnectV2)}>
                         Wallet Connect
                     </button> */}
