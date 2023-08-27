c05617c1dae1027fe81d64a718d3e2a553f02e16
lelkovic <gladv1762@gmail.com>
2023-08-27 13:32:20 +0300

    profile menu upd

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -51,8 +51,12 @@ export const Header = () => {
             {isMobileMenu && (
                 <div className={styles.mobileMenu}>
                     <nav>
-                        <a href="#Discover">Discover</a>
-                        <a href="#How-it-work">How it work</a>
+                        <a className={styles.header__ml32} href="#Discover">
+                            Discover
+                        </a>
+                        <a className={styles.header__ml32} href="#How-it-work">
+                            How it work
+                        </a>
                     </nav>
                     <div className={styles.mobileMenu__bottom}>
                         <button className={styles.mobileMenu__walletButton}>Connect Wallet</button>
--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -4,15 +4,45 @@ import styles from "./styles.module.scss";
 import avatar from "../../assets/images/avatar.png";
 
 export const Profile = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={styles.profile}>
-            <img src={avatar} alt="avatar" className={styles.profile__img} />
-            <p className={styles.profile__balance}>123</p>
-            <p className={styles.profile__eth}>ETH</p>
+            <button className={styles.profile__button} onClick={() => setIsOpen(!isOpen)}>
+                <img src={avatar} alt="avatar" className={styles.profile__img} />
+                <p className={styles.profile__balance}>123</p>
+                <p className={styles.profile__eth}>ETH</p>
+            </button>
+            {isOpen && (
+                <div className={styles.profile__menu}>
+                    <p className={styles.profile__userName}>Enrico Cole</p>
+                    <p className={styles.profile__address}>Wallet</p>
+                    <div className={styles.profile__chainInfo}>
+                        <div className={styles.profile__chain}>
+                            <img src={avatar} alt="avatar" className={styles.profile__img} />
+                        </div>
+                        <div className={styles.profile__chainBalance}>
+                            <p className={styles.profile__chainTitle}>Balance</p>
+                            <p className={styles.profile__chainValue}>4.689 ETH</p>
+                        </div>
+                        <a className={styles.profile__linkCoinbase} href="#">
+                            Manage fun on Coinbase
+                        </a>
+                    </div>
+                    <div className={styles.profile__item}>
+                        <a href="#">My Profile</a>
+                    </div>
+                    <div className={styles.profile__item}>
+                        <a href="#">My Items</a>
+                    </div>
+                    <div className={styles.profile__item}>
+                        <p>Dark Theme</p>
+                    </div>
+                    <div className={styles.profile__item}>
+                        <button onClick={handleDisconnect}>Disconnect</button>
+                    </div>
+                </div>
+            )}
         </div>
-        //    <button className={styles.header__wbtn} onClick={handleDisconnect}>
-        //         Disconnect
-        //     </button>
     );
 };
 export default Profile;
