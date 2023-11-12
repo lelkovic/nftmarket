74ec3e1725107c0b333779c12df05c6e288f08ed
lelkovic <gladv1762@gmail.com>
2023-11-12 15:33:15 +0100

    Profile Gallery upd

--- src/components/ProfileGallery/index.js
+++ src/components/ProfileGallery/index.js
@@ -1,22 +1,22 @@
 import React from "react";
 import styles from "./styles.module.scss";
+import { useState } from "react";
 
 export const ProfileGallery = () => {
-    const [balances, setBalances] = useState();
+    const [activeTab, setActiveTab] = useState("On Sale");
     return (
         <div className={styles.ProfileGallery}>
             <div className={styles.ProfileGallery__tabs}>
-                <button className={styles.ProfileGallery__activeTab}>On Sale</button>
+                <button className={activeTab === "On Sale" && styles.ProfileGallery__activeTab}>On Sale</button>
                 <button>Collectibles</button>
                 <button>Created</button>
                 <button>Likes</button>
                 <button>Following</button>
                 <button>Followers</button>
-                
-                <div/>
+            </div>
             <div className={styles.ProfileGallery__galleryWrap}></div>
 
-            <div className={ProfileGallery__loader}></div>
+            <div className={styles.ProfileGallery__loader}></div>
         </div>
     );
 };
