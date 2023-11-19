4007b1b2edbcf9746e2ae89feb146c6ea1331c8f
lelkovic <gladv1762@gmail.com>
2023-11-19 15:32:13 +0100

    Profile Gallery upd

--- src/components/ProfileGallery/index.js
+++ src/components/ProfileGallery/index.js
@@ -1,20 +1,75 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import { useState } from "react";
+import data from "./gallery.json";
+import profileBackground from "../../assets/images/profilebackground.png";
 
 export const ProfileGallery = () => {
+    const gallery = data.gallery;
     const [activeTab, setActiveTab] = useState("On Sale");
+    const renderGallery = () => {
+        return gallery.map((item, key) => (
+            <div className={styles.ProfileGallery__item}>
+                <img className={styles.ProfileGallery__itemImg} src={item.img} />
+                <div className={styles.ProfileGallery__itemBlock}>
+                    <h3>{item.title}</h3>
+                    <p className={styles.ProfileGallery__itemPrice}>{item.price} ETH</p>
+                </div>
+                <div className={styles.ProfileGallery__itemBlock}>
+                    <div></div>
+                    <p>{item.stock.length} in stock </p>
+                </div>
+                <div className={styles.ProfileGallery__sep}></div>
+                <div className={styles.ProfileGallery__itemBlock}>
+                    <p>
+                        Highest bit <b>{item.highestBid} ETH</b>{" "}
+                    </p>
+                    <p> New bid 🔥</p>
+                </div>
+            </div>
+        ));
+    };
     return (
         <div className={styles.ProfileGallery}>
             <div className={styles.ProfileGallery__tabs}>
-                <button className={activeTab === "On Sale" && styles.ProfileGallery__activeTab}>On Sale</button>
-                <button>Collectibles</button>
-                <button>Created</button>
-                <button>Likes</button>
-                <button>Following</button>
-                <button>Followers</button>
+                <button
+                    onClick={() => setActiveTab("On Sale")}
+                    className={activeTab === "On Sale" && styles.ProfileGallery__activeTab}
+                >
+                    On Sale
+                </button>
+                <button
+                    onClick={() => setActiveTab("Collectibles")}
+                    className={activeTab === "Collectibles" && styles.ProfileGallery__activeTab}
+                >
+                    Collectibles
+                </button>
+                <button
+                    onClick={() => setActiveTab("Created")}
+                    className={activeTab === "Created" && styles.ProfileGallery__activeTab}
+                >
+                    Created
+                </button>
+                <button
+                    onClick={() => setActiveTab("Likes")}
+                    className={activeTab === "Likes" && styles.ProfileGallery__activeTab}
+                >
+                    Likes
+                </button>
+                <button
+                    onClick={() => setActiveTab("Following")}
+                    className={activeTab === "Following" && styles.ProfileGallery__activeTab}
+                >
+                    Following
+                </button>
+                <button
+                    onClick={() => setActiveTab("Followers")}
+                    className={activeTab === "Followers" && styles.ProfileGallery__activeTab}
+                >
+                    Followers
+                </button>
             </div>
-            <div className={styles.ProfileGallery__galleryWrap}></div>
+            <div className={styles.ProfileGallery__galleryWrap}>{renderGallery()}</div>
 
             <div className={styles.ProfileGallery__loader}></div>
         </div>
