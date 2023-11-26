daead45ca1bf545bab2ffe7e7433dea4dd0a1d66
lelkovic <gladv1762@gmail.com>
2023-11-26 15:02:52 +0100

    Profile Gallery upd

--- src/components/ProfileGallery/index.js
+++ src/components/ProfileGallery/index.js
@@ -2,29 +2,36 @@ import React from "react";
 import styles from "./styles.module.scss";
 import { useState } from "react";
 import data from "./gallery.json";
-import profileBackground from "../../assets/images/profilebackground.png";
 
 export const ProfileGallery = () => {
     const gallery = data.gallery;
     const [activeTab, setActiveTab] = useState("On Sale");
+    console.log(data.gallery);
+    const renderStocks = (stock) => {
+        return stock.map((stockImage, key) => (
+            <div key={key} className={styles.ProfileGallery__stock}>
+                <img className={styles.ProfileGallery__stockImg} src={stockImage} />
+            </div>
+        ));
+    };
     const renderGallery = () => {
         return gallery.map((item, key) => (
-            <div className={styles.ProfileGallery__item}>
+            <div key={key} className={styles.ProfileGallery__item}>
                 <img className={styles.ProfileGallery__itemImg} src={item.img} />
                 <div className={styles.ProfileGallery__itemBlock}>
                     <h3>{item.title}</h3>
                     <p className={styles.ProfileGallery__itemPrice}>{item.price} ETH</p>
                 </div>
                 <div className={styles.ProfileGallery__itemBlock}>
-                    <div></div>
-                    <p>{item.stock.length} in stock </p>
+                    <div className={styles.ProfileGallery__stocksWrap}>{renderStocks(item.stock)}</div>
+                    <p className={styles.ProfileGallery__textStock}>{item.stock.length} in stock </p>
                 </div>
                 <div className={styles.ProfileGallery__sep}></div>
                 <div className={styles.ProfileGallery__itemBlock}>
-                    <p>
-                        Highest bit <b>{item.highestBid} ETH</b>{" "}
+                    <p className={styles.ProfileGallery__bidInfo}>
+                        Highest bit <b>{item.highestBid} ETH</b>
                     </p>
-                    <p> New bid 🔥</p>
+                    <p className={styles.ProfileGallery__bidInfo}> New bid 🔥</p>
                 </div>
             </div>
         ));
